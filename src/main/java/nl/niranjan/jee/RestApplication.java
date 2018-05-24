0c9c9a51a3f75709dd2592ddf37fbfc5c4cfd85c
Niranjan Rath <doddegras@gmail.com>
2018-05-25 00:29:03 +0200

    inital commit

---
+++ src/main/java/nl/niranjan/jee/RestApplication.java
@@ -0,0 +1,8 @@
+package nl.niranjan.jee;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class RestApplication extends Application {
+}
---
+++ src/main/java/nl/niranjan/jee/TestEndPoint.java
@@ -0,0 +1,17 @@
+package nl.niranjan.jee;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/test")
+public class TestEndPoint {
+
+    @GET
+    @Produces({"text/plain"})
+    @Path("/hello")
+    public String listPaperBooks() {
+        return "Hello World !!";
+    }
+
+}
